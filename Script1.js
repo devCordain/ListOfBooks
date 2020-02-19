2a96e1a712835d239a4ea824c847af1f2ecf73a4
Tommy Karlsson <pctkar@gmail.com>
2020-02-19 09:53:48 +0100

    added localstorage

--- Script1.js
+++ Script1.js
@@ -1,72 +1,21 @@
-// JavaScript source code
-
-//Test function
-//function myFunction() {
-//    document.body.style.backgroundColor = "red";
-//}
-
-
-////https://www.youtube.com/watch?time_continue=85&v=FN_ffvw_ksE&feature=emb_logo
-
-////get data 
-//function FetchData() {
-//    fetch("https:/reqres.in/api/users")
-//        .then(Response => {
-//            return Response.json();
-//        }).then(data => {
-//            //console.log(data.data);
-//            const html = data.data.map(user => {
-//                return `<p>Name: ${user.first_name}</p>`
-//            }).join('');
-//            document.querySelector('#users')
-//                .insertAdjacentHTML('afterbegin', html);
-//        }).catch(error => {
-//            console.log(error);
-//        })
-//}
-
-////Post Data
-//function PostData() {
-//    fetch("https:/reqres.in/api/users", {
-//        method: 'POST',
-//        headers: {
-//            "Content-Type": "application/json"
-//        },
-//        body: JSON.stringify({
-//            name: 'Petter',
-//            job: 'Developer'
-//        })
-//    })
-//        .then(Response => {
-//            return Response.json();
-//        }).then(data => {
-//            console.log(data);
-//        }).catch(error => {
-//            console.log(error);
-//        })
-//}
-
-
-//const Http = new XMLHttpRequest();
-//const url = 'https://www.forverkliga.se/JavaScript/api/crud.php?requestKey';
-//Http.open("GET", url);
-//Http.send();
-
-
-//Http.onreadystatechange = (e) => {
-//    var response = JSON.parse(Http.responseText);
-//    alert(response.key);
-//}                       
 
 
 var xhReq = new XMLHttpRequest();
-xhReq.open("GET", 'https://www.forverkliga.se/JavaScript/api/crud.php?requestKey', false);
-xhReq.send(null);
-var jsonObject = JSON.parse(xhReq.responseText);
+
+var key = localStorage.getItem('apiAccessKey');
+alert(key);
+if (key == null) {
+    xhReq.open("GET", 'https://www.forverkliga.se/JavaScript/api/crud.php?requestKey', false);
+    xhReq.send(null);
+    var jsonObject = JSON.parse(xhReq.responseText);
+    key = jsonObject.key.toString();  
+    localStorage.setItem('apiAccessKey', key);
+}
 
 console.log(jsonObject.key);
 
-let key = jsonObject.key.toString();  
+
+
 const baseUrl = 'https://www.forverkliga.se/JavaScript/api/crud.php?key=' + key;
 
 const insertBook = baseUrl + '&op=insert';
@@ -78,10 +27,6 @@ console.log(insertBook);
 console.log(getBooks);
 console.log(updateBook);
 console.log(deleteBook); 
-
-
-
-//https://www.google.com/maps/embed/v1/MODE?key=YOUR_API_KEY&parameters
 
 const mytitle = "another title";
 const myauthor = "another author";
